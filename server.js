fd3807503168370d810e690be37b697483ba44e6
Dev-Kevin012 <142422385+Dev-Kevin012@users.noreply.github.com>
2023-09-04 21:48:44 +0530

    Task 2

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
-app.use(express.json());
-app.use((req, res, next) => {
-  console.log("In the MiddleWare!");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(`<form action="/add" method="post">
+      <input type="text" name="title" />
+      <input type="text" name="size" />
+      <button type="submit">Add Product</button>
+    </form>`);
 });
+
+app.use("/add", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res) => res.send("<h1>Hello World!<h1>"));
 
 app.listen(3000, () => console.log("Server is Running!"));
